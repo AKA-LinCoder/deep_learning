e5065e401a97269d9570f4490b571b0a30ce8ce5
lin <2423860442@qq.com>
2025-05-13 11:00:10 +0800

    定位头部

--- image_location.py
+++ image_location.py
@@ -16,9 +16,40 @@ from lxml import etree
 
 batch_size = 8
 pil_img = Image.open(r"dataset/images/Abyssinian_1.jpg")
+np_img = np.array(pil_img)
 print(np.array(pil_img).shape)
 xml = open(r"dataset/annotations/xmls/Abyssinian_1.xml").read()
 print(xml)
 sel = etree.HTML(xml)
-width = sel.xpath("//size/width/text()")[0]
+width = int(sel.xpath("//size/width/text()")[0])
+height = int(sel.xpath("//size/height/text()")[0])
 print(width)
+
+ymax = int(sel.xpath("//bndbox/ymax/text()")[0])
+
+ymin = int(sel.xpath("//bndbox/ymin/text()")[0])
+xmax = int(sel.xpath("//bndbox/xmax/text()")[0])
+xmin = int(sel.xpath("//bndbox/xmin/text()")[0])
+
+
+
+plt.imshow(np_img)
+
+rect = Rectangle((xmin,ymin),(xmax-xmin),(ymax-ymin),fill=False,color="red")
+ax = plt.gca()
+ax.axes.add_patch(rect)
+plt.show()
+
+img = pil_img.resize((224,224))
+xmin = (xmin/width)*224
+ymin = (ymin/height)*224
+xmax= (xmax/width)*224
+ymax = (ymax/height)*224
+
+
+plt.imshow(img)
+
+rect = Rectangle((xmin,ymin),(xmax-xmin),(ymax-ymin),fill=False,color="blue")
+ax = plt.gca()
+ax.axes.add_patch(rect)
+plt.show()
